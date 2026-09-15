3adb5262aabfd39c7d8eb793a5e4881ef0c908d4
marco <marcosffhotmail.com@gmail.com>
2026-09-15 18:49:07 -0300

    feat: add initial menu screen

--- src/main/java/corefall/engine/GamePanel.java
+++ src/main/java/corefall/engine/GamePanel.java
@@ -17,6 +17,7 @@ public class GamePanel extends JPanel implements Runnable {
             Constants.SCREEN_WIDTH / 2.0 - 15,
             Constants.SCREEN_HEIGHT / 2.0 - 15);
     private Thread gameThread;
+    private GameState gameState = GameState.MENU;
 
     public GamePanel() {
 
@@ -56,9 +57,53 @@ public class GamePanel extends JPanel implements Runnable {
 
         Graphics2D g2 = (Graphics2D) g;
 
-        player.draw(g2);
+        switch (gameState) {
+            case MENU -> drawMenu(g2);
+            case PLAYING -> player.draw(g2);
+            default -> {}
+        }
 
         g2.dispose();
 
+    }
+
+    private void drawMenu(Graphics2D g2) {
+
+    g2.setColor(Color.WHITE);
+
+    // Título
+    g2.setFont(new java.awt.Font("SansSerif", java.awt.Font.BOLD, 52));
+
+    String title = "COREFALL";
+    var metrics = g2.getFontMetrics();
+
+    int x = (Constants.SCREEN_WIDTH - metrics.stringWidth(title)) / 2;
+    int y = 220;
+
+    g2.drawString(title, x, y);
+
+    // Linhas laterais
+    g2.drawLine(x - 90, y - 18, x - 20, y - 18);
+    g2.drawLine(x + metrics.stringWidth(title) + 20, y - 18,
+            x + metrics.stringWidth(title) + 90, y - 18);
+
+    // Botão Play
+    int bw = 220;
+    int bh = 46;
+
+    int bx = (Constants.SCREEN_WIDTH - bw) / 2;
+    int by = 420;
+
+    g2.drawRect(bx, by, bw, bh);
+
+    g2.setFont(new java.awt.Font("Monospaced", java.awt.Font.PLAIN, 20));
+
+    String play = "PLAY";
+    var playMetrics = g2.getFontMetrics();
+
+    g2.drawString(
+            play,
+            bx + (bw - playMetrics.stringWidth(play)) / 2,
+            by + 29);
     }
 }
---
+++ src/main/java/corefall/engine/GameState.java
@@ -0,0 +1,9 @@
+package corefall.engine;
+
+public enum GameState {
+    MENU,
+    START_SELECTION,
+    PLAYING,
+    LEVEL_UP,
+    GAME_OVER
+}
